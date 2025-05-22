39b1a2fc28050189e2fd6220bb43253dcdd1555b
Simo <medkehalofficiel@gmail.com>
2025-05-22 22:37:20 +0100

    PUSHH SIMO

--- include/nav_front.php
+++ include/nav_front.php
@@ -55,7 +55,7 @@ if ($connecte) {
             <a class="nav-link" href="../deconnexion.php" onclick="return confirm('Se déconnecter ?');">Déconnexion</a>
           </li>
         <?php else: ?>
-          <li class="nav-item"><a class="nav-link" href="../login.php">Connexion</a></li>
+          <li class="nav-item"><a class="nav-link" href="../connexion.php">Connexion</a></li>
         <?php endif; ?>
       </ul>
     </div>
